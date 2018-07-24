4d1796b5a9392615d994a2044184de90a8948dd8
Trav Colbert <trav.colbert@gmail.com>
2018-07-24 15:13:29 -0700

    fixed associations on froogle tables

--- models/associations/froogle_associations.js
+++ models/associations/froogle_associations.js
@@ -2,10 +2,10 @@ module.exports = function(app) {
   (function() {
     let myName = "froogle expenses";
     app.log("Invoking association",myName,6);
-    app.models["expenses"].hasOne(app.models["categories"],{as:"categoryId"});  // makes expenses.categoryId -> categories.id
-    app.models["expenses"].hasOne(app.models["users"]);                         // makes expenses.userId
-    app.models["expenses"].hasOne(app.models["domains"]);                       // makes expenses.domainId
-    app.models["categories"].hasOne(app.models["domains"]);                     // makes categories.domainId
+    app.models["expenses"].belongsTo(app.models["categories"],{as:"category"});  // makes expenses.categoryId -> categories.id
+    app.models["expenses"].belongsTo(app.models["users"]);                         // makes expenses.userId
+    app.models["expenses"].belongsTo(app.models["domains"]);                       // makes expenses.domainId
+    app.models["categories"].belongsTo(app.models["domains"]);                     // makes categories.domainId
     // app.models["expenses"].hasOne(app.models["users"],{as:"user"});             // makes expenses.userId
     // app.models["parts"].belongsToMany(app.models["cars"],{through:"PartCar"});  // makes new model 'PartCar' connecting the two
   })(app);
