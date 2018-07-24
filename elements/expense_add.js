ddb3e7d58d237e24b3a4bde7627d289e46bf6f5b
Trav Colbert <trav.colbert@gmail.com>
2018-07-24 15:52:52 -0700

    fleshing-out expenses

---
+++ elements/expense_add.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["create","all"],
+  element:{
+    id:"expense_add_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttonexpenseadd"],
+    content:"<i class=\"material-icons\">add_circle_outline</i><span>New Expense</span>"
+  }
+}
