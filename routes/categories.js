f1b5c53ebb2b2eba9d9a1f115e08ce07c6859135
Trav Colbert <trav.colbert@gmail.com>
2018-07-25 15:38:43 -0700

    routes created for froogle

--- routes/categories.js
+++ routes/categories.js
@@ -4,7 +4,7 @@ var router = express.Router();
 module.exports = function(app) {
   router.get('/',app.tools.checkAuthentication,app.controllers["categories"].gets);
   router.get('/:id/',app.tools.checkAuthentication,app.controllers["categories"].get);
-  router.get('/:id/actions/edit',app.tools.checkAuthentication,app.controllers["categories"].editCategoryForm);
+  router.get('/:id/actions/edit/',app.tools.checkAuthentication,app.controllers["categories"].editCategoryForm);
   router.post('/',app.tools.checkAuthentication,app.controllers["categories"].createCategory);
   return router;
 };
