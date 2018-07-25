8bfb44ecac46a54cccdee4c6bd3d599089fac441
Trav Colbert <trav.colbert@gmail.com>
2018-07-25 15:39:56 -0700

    added authorized buttons

---
+++ elements/category_edit.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"category_edit_button",
+    element:"a",
+    classlist:["btn","tertiary","layout-size-x1","buttoncategoryedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit Category</span>"
+  }
+};
