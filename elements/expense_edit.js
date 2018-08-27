cea00bd1fdeb023098d1c9e45d95031fc5f88a7e
Trav Colbert <trav.colbert@gmail.com>
2018-08-27 08:28:53 -0700

    edit button

---
+++ elements/expense_edit.js
@@ -0,0 +1,9 @@
+module.exports = {
+  role:["edit","all"],
+  element:{
+    id:"expense_edit_button",
+    element:"a",
+    classlist:["btn","secondary","layout-size-x1","buttonexpenseedit"],
+    content:"<i class=\"material-icons\">mode_edit</i><span>Edit Expense</span>"
+  }
+}
